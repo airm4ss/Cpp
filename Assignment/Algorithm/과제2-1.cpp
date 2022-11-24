af9781b89d8efcf8fffefba9f3592ee43edcc6a3
mmingidan <102302841+mmingidan@users.noreply.github.com>
2022-11-24 15:51:58 +0900

    과제 2-1 (배열 생성)
    컴퓨터알고리즘 과제

---
+++ Assignment/Algorithm/과제2-1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A[5][5];  //2차원 배열 A
+	int N = 1, M = 5, V = 0;  //M은 배열 대각선 갯수의 중간값
+
+	//V+=1 순서도에서는 이 위치에 삽입되어야 하지만 C++은 순서도와 다르게 배열 행, 열 번호가 0부터 시작하기 때문에 아래 위치에 삽입
+	for (int i = 0; i < M; i++) {
+		for (int j = i; j >= 0; j--) {
+			int C = j;
+			int R = V - j;
+			A[C][R] = N;
+			N += 1;
+		}
+		V += 1;
+	}
+
+	//V+=1 순서도에서는 이 위치에 삽입되어야 하지만 C++은 순서도와 다르게 배열 행, 열 번호가 0부터 시작하기 때문에 아래 위치에 삽입
+	for (int i = 1; i < M; i++) {
+		for (int j = 4; j >= i; j--) {
+			int C = j;
+			int R = V - j;
+			A[C][R] = N;
+			N += 1;
+		}
+		V += 1;
+	}
+
+	for (int i = 0; i < 5; i++) {  //배열A 출력
+		for (int j = 0; j < 5; j++)
+			cout << "  " << A[i][j];
+		cout << endl;
+	}
+}
